5527520d52f994b03efd95b054aa26ea0a1ff927
Kentaro Hayashi <hayashi@clear-code.com>
2017-10-02 14:30:06 +0900

    Import template files

---
+++ lib/colleagues/calendar/input/helper.rb
@@ -0,0 +1,11 @@
+require "colleagues/calendar/input/helper/version"
+
+module Colleagues
+  module Calendar
+    module Input
+      module Helper
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/colleagues/calendar/input/helper/version.rb
@@ -0,0 +1,9 @@
+module Colleagues
+  module Calendar
+    module Input
+      module Helper
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
